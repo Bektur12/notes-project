627a66a8f31590cf620fc094a2fb5b8b28779da1
Bektur12 <kanybekovbektur22@gmail.com>
2023-04-23 04:56:34 +0600

    add input-password

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import { InputPassword } from "./components/UI/InputPassword";
+import { Input } from "./components/UI/Input";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <InputPassword />
+      <Input />
+    </div>
+  );
 }
 
 export default App;
