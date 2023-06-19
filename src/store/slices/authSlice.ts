d0d2818478aa82995dd4f9a826b6890f6803118c
Bektur12 <kanybekovbektur22@gmail.com>
2023-06-19 16:43:32 +0600

    bugfix auth

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -2,15 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 import { loginUser } from "../actions/auth";
 
 const initialState = {
-  id: "",
+  user: {
+    id: "",
+    username: "",
+  },
 };
 export const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    baseAuth(state, action) {
+      const user = action.payload;
+      state.user.id = user.id;
+      state.user.username = user.firstName;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(loginUser.fulfilled, (state, action) => {
-      state.id = action.payload;
+      // state.id = action.payload;
     });
   },
 });
+export const { baseAuth } = authSlice.actions;
+export default authSlice;
